594ba5c28a372cb1ae6135909885b802725f3a19
Buncy <buncy.shaddai@oneconvergence.com>
2023-04-03 05:36:16 +0000

    single node rke2 cluster

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo systemctl disable apparmor.service
+sudo systemctl disable firewalld.service
+sudo systemctl disable ufw
+sudo systemctl stop apparmor.service
+sudo systemctl stop firewalld.service
+sudo systemctl stop ufw
+sudo systemctl disable swap.target
+sudo swapoff -a
+
+
+sudo sed -i -e "/AllowAgentForwarding/,/VersionAddendum/s/.*AllowTcpForwarding.*/AllowTcpForwarding yes/" /etc/ssh/sshd_config
+sudo sed -i -e "/AllowAgentForwarding/,/VersionAddendum/s/.*PermitTunnel.*/PermitTunnel yes/" /etc/ssh/sshd_config
+
+sudo service sshd restart
+
+
+curl -sfL https://get.rke2.io | sudo sh
+sudo mkdir -p /etc/rancher/rke2
+sudo cp ./config.yaml /etc/rancher/rke2
+sudo systemctl enable rke2-server.service
+sudo  systemctl start rke2-server.service
+
+cp /var/lib/rancher/rke2/bin/kubectl /usr/local/bin
+
+export PATH=$PATH:/opt/rke2/bin:/var/lib/rancher/rke2/bin
+
+export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
