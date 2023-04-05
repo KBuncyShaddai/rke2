d1b717b15807783d33f883426d9a68811bddbaba
Your Name <buncy.shaddai@oneconvergence.com>
2023-04-05 21:17:45 +0000

    updated rke2.sh

--- rke2.sh
+++ rke2.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
+
+
+MASTER_SSH_KEY=/home/ubuntu/keys/104/jenkins-rke.pem
+MASTER_SSH_USER=dkube
+MASTER_NODE=192.168.200.104
+CONFIG_FILE=./config.yaml
+
+AGENT_NODES=()
+AGENT_SSH_KEYS=()
+AGENT_SSH_USER=()
+AGENT_NODE_TYPE=()
+
 function white_printf  { printf "\t\033[1;37m$@\033[0m";  }
 function red_printf    { printf "\t\033[31m$@\033[0m";    }                                                               #
 function green_printf  { printf "\t\033[32m$@\033[0m";    }                                                               #
 function yellow_printf { printf "\t\033[33m$@\033[0m";    }
 
-SSH_KEY=/home/ubuntu/keys/104/jenkins-rke.pem
-SSH_USER=dkube
-NODE=192.168.200.104
-
-
 function setAgentConfig {
-	white_printf	"Fetching token from Server Node......\n"
-	token=$(ssh -i $SSH_KEY $SSH_USER@$NODE sudo cat /var/lib/rancher/rke2/server/node-token)
-  	green_printf "Token fetched\n"
-	cat config.yaml > agent-config.yaml
-	echo server: https://$NODE:9345 >> agent-config.yaml
-	echo token: $token >> agent-config.yaml
-	green_printf "Configured agent-config.yaml.\n"
+  white_printf	"Fetching token from Server node $NODE.\n"
+  token=$(ssh -i $SSH_KEY $SSH_USER@$NODE sudo cat /var/lib/rancher/rke2/server/node-token)
+  green_printf "Token fetched.\n"
+  cat config.yaml > agent-config.yaml
+  echo server: https://$NODE:9345 >> agent-config.yaml
+  echo token: $token >> agent-config.yaml
+  green_printf "Configured agent-config.yaml.\n"
 }
 
-function installAgent {
-	NODE=192.168.200.107
-	scp -i $SHH_KEY ./agent-config  $SSH_USER@$NODE:/etc/rancher/rke2/config.yaml 
-	ssh -i $SSH_KEY $SSH_USER@$NODE 'bash -s' < install.sh agent
+function copyConfig {
+  scp -i $SSH_KEY $CONFIG_FILE  $SSH_USER@$NODE:/tmp/config.yaml
+  ssh -i $SSH_KEY $SSH_USER@$NODE sudo cp /tmp/config.yaml /etc/rancher/rke2/config.yaml
+  green_printf "Copied config to $NODE_TYPE node $NODE.\n"
 }
 
-setAgentConfig
-installAgent
+function installRKE2 {
+	ssh -i $SSH_KEY $SSH_USER@$NODE 'bash -s' < install.sh $NODE_TYPE
+}
+
+function setupMasterNode {
+  SSH_USER=$MASTER_SSH_USER
+  SSH_KEY=$MASTER_SSH_KEY
+  NODE=$MASTER_NODE
+  NODE_TYPE=server
+
+  copyConfig
+  installRKE2
+}
+
+setupMasterNode
