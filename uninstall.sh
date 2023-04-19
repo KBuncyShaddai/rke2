85eb8df41c1db93e087f2f91e24f2c8befd57ec7
KBuncyShaddai <buncy.shaddai@oneconvergence.com>
2023-04-19 15:05:06 +0530

    Added images and iptbales cleanup

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,4 @@
 sudo /usr/local/bin/rke2-killall.sh
 sudo /usr/local/bin/rke2-uninstall.sh
+curl -s https://raw.githubusercontent.com/rancherlabs/support-tools/master/extended-rancher-2-cleanup/extended-cleanup-rancher2.sh | sudo bash -i -f
+
