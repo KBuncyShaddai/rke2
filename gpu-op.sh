05540ede0284a6bba3f34e83a7a9e4aae7cdd2f1
Your Name <buncy.shaddai@oneconvergence.com>
2023-04-22 14:37:20 +0000

    added gpu-op.sh

--- gpu-op.sh
+++ gpu-op.sh
@@ -25,7 +25,4 @@ version = 2
 EOF
 CONTAINERD_SOCKET=/run/k3s/containerd/containerd.sock
 
-echo "\t Copied nvidia runtime config to containerd config\n"
-echo  "\tHelm installing nvidia gpu operator\n"
-helm repo add nvidia https://helm.ngc.nvidia.com/nvidia && helm repo update
-helm install gpu-operator --wait  -n gpu-operator --create-namespace    nvidia/gpu-operator --set toolkit.env[0].name=CONTAINERD_CONFIG     --set toolkit.env[0].value=/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl     --set toolkit.env[1].name=CONTAINERD_SOCKET     --set toolkit.env[1].value=/run/k3s/containerd/containerd.sock     --set toolkit.env[2].name=CONTAINERD_RUNTIME_CLASS     --set toolkit.env[2].value=nvidia     --set toolkit.env[3].name=CONTAINERD_SET_AS_DEFAULT     --set-string toolkit.env[3].value=true 
+echo "\t Copied nvidia runtime config to containerd config $CONTAINERD_CONFIG\n"
