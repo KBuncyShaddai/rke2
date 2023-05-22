2bbef5d3c8b1675cb31ea25d7fbb90b1aefed749
Buncy Shaddai <116257781+KBuncyShaddai@users.noreply.github.com>
2023-05-22 11:31:34 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,15 +8,15 @@ sudo /usr/local/bin/rke2-uninstall.sh
 
 #RKE2 Cleanup
 
-systemctl stop rancher-system-agent.service
-systemctl disable rancher-system-agent.service
-rm -f /etc/systemd/system/rancher-system-agent.service
-rm -f /etc/systemd/system/rancher-system-agent.env
-systemctl daemon-reload
-rm -f /usr/local/bin/rancher-system-agent
-rm -rf /etc/rancher/
-rm -rf /var/lib/rancher/
-rm -rf /usr/local/bin/rke2*
+sudo systemctl stop rancher-system-agent.service
+sudo systemctl disable rancher-system-agent.service
+sudo rm -f /etc/systemd/system/rancher-system-agent.service
+sudo rm -f /etc/systemd/system/rancher-system-agent.env
+sudo systemctl daemon-reload
+sudo rm -f /usr/local/bin/rancher-system-agent
+sudo rm -rf /etc/rancher/
+sudo rm -rf /var/lib/rancher/
+sudo rm -rf /usr/local/bin/rke2*
 
 #DOCKER CLEANUP
 
